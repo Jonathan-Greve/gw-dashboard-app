74bccffb47f4ec9d2f68fee7c1397e134f9f64a6
Jonathan-Greve <jonagrev@gmail.com>
2022-02-01 00:51:16 +0800

    Adde protobuf files

---
+++ src/Protobuf Files/profession.js
@@ -0,0 +1,32 @@
+// source: Profession.proto
+/**
+ * @fileoverview
+ * @enhanceable
+ * @suppress {missingRequire} reports error on implicit type usages.
+ * @suppress {messageConventions} JS Compiler reports an error if a variable or
+ *     field starts with 'MSG_' and isn't a translatable message.
+ * @public
+ */
+// GENERATED CODE -- DO NOT EDIT!
+/* eslint-disable */
+// @ts-nocheck
+
+goog.provide('proto.gw_messages.Profession');
+
+/**
+ * @enum {number}
+ */
+proto.gw_messages.Profession = {
+  NONE: 0,
+  WARRIOR: 1,
+  RANGER: 2,
+  MONK: 3,
+  NECROMANCER: 4,
+  MESMER: 5,
+  ELEMENTALIST: 6,
+  ASSASSIN: 7,
+  RITUALIST: 8,
+  PARAGON: 9,
+  DERVISH: 10
+};
+
